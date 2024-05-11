3383fa6b1f0f211e2152815c1b2165106f1551fe
李明扬 <1342554547@qq.com>
2024-05-11 10:34:07 +0800

    ⚡️【优化】逻辑优化

--- src/pages/tabbar/tabbar-1/index.vue
+++ src/pages/tabbar/tabbar-1/index.vue
@@ -1,9 +1,54 @@
 <template>
-  <view class=""></view>
+  <view class="test">
+    <view class="item" v-for="(item, index) in 50" :key="index"></view>
+  </view>
+  <!-- <zm-list ref="listRef" :options="options" @query="handleGetData">
+    <view v-for="(item, index) in list" :key="index" class="list-item">{{ index }}</view>
+  </zm-list> -->
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
+import { onPageScroll, onReachBottom } from "@dcloudio/uni-app"
+// 监听滚动触底
+onReachBottom(() => {
+  // console.log("zm-view.onReachBottom")
+})
+
+onPageScroll(() => {})
+
+const list = ref([])
+const options = reactive({ size: 15 })
+function handleGetData({ page, pageSize, type, success }) {
+  if (type == "load") {
+    setTimeout(() => {
+      const l = Array.from({ length: pageSize }, (_, i) => i)
+      list.value = list.value.concat(l)
+      success({ list: l, total: 80 })
+    }, 100)
+  }
+  if (type == "refresh") {
+    setTimeout(() => {
+      const l = Array.from({ length: pageSize }, (_, i) => i)
+      list.value = l
+      success({ list: l, total: 80 })
+    }, 1000)
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+  height: 100%;
+}
+
+.test {
+  height: 200vh;
+}
+
+.item {
+  height: 80rpx;
+  margin: 30rpx;
+  border: 1px solid red;
+}
+</style>
--- src/ui/zm-view/zm-view.vue
+++ src/ui/zm-view/zm-view.vue
@@ -4,7 +4,8 @@
   </view>
 </template>
 <script setup lang="ts">
-import { useMitt, useStyle, useCurrentRouter } from "../hooks"
+import { useMitt, useStyle } from "../hooks"
+
 const emits = defineEmits(["scroll", "scrolltoupper", "scrolltolower", "touchstart", "touchmove", "touchend"])
 const props = defineProps({
   scroll: { type: Boolean, default: true },
@@ -12,8 +13,8 @@ const props = defineProps({
   customClass: { type: String, default: "" },
   customStyle: { type: [String, Object], default: "" },
 })
+
 const mitt = useMitt()
-const router = useCurrentRouter()
 
 // 自定义样式
 const style = computed(() => {
@@ -22,37 +23,18 @@ const style = computed(() => {
   return useStyle({ ...style, ...useStyle(props.customStyle) })
 })
 
-// 监听全局事件
-onGlobalEvents()
-function onGlobalEvents() {
-  // 监听页面滚动
-  uni.$on("onPageScroll", (e) => {
-    // 滚动事件
-    emits("scroll", e.scrollTop, { route: router.route })
-    this.mitt.emit("scroll", e.scrollTop, { route: router.route })
-    // 滚动触顶事件
-    if (e.scrollTop === 0) {
-      emits("scrolltoupper", { route: router.route })
-      this.mitt.emit("scrolltoupper", { route: router.route })
-    }
-  })
-  // 监听页面滚动触底
-  uni.$on("onReachBottom", () => {
-    emits("scrolltolower")
-    this.mitt.emit("scrolltolower")
-  })
-}
-
 // 屏幕按下
 function onTouchstart(e: Event) {
   emits("touchstart", e)
   mitt.emit("touchstart", e)
 }
+
 // 屏幕松开
 function onTouchend(e: Event) {
   emits("touchend", e)
   mitt.emit("touchend", e)
 }
+
 // 屏幕滑动
 function onTouchmove(e: Event) {
   emits("touchmove", e)
