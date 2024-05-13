d73c0052329fe67f057e3141973aa156719682d8
李明扬 <1342554547@qq.com>
2024-05-13 14:20:42 +0800

    ⚡️【优化】组件转换

--- src/ui/zm-text-highlight/zm-text-highlight.vue
+++ src/ui/zm-text-highlight/zm-text-highlight.vue
@@ -1,122 +1,77 @@
 <template>
   <view class="zm-text-highlight" :style="[style]" :class="[customClass]">
-    <text v-for="(item, index) in list" :key="index" :style="[textStyle(item)]" class="zm-text-highlight__text" :class="{ 'is-matched': text && text.matched }">{{ item.text }}</text>
+    <text v-for="(item, index) in list" :key="index" :style="[textStyle(item)]" class="zm-text-highlight__text" :class="{ 'is-matched': text && item.matched }">
+      {{ item.text }}
+    </text>
   </view>
 </template>
 
-<script>
-import hook from "@/mixins/hook"
-import weixin from "@/mixins/weixin"
-import { isNoEmpty } from "@/utils/check"
-import { useStyle, useColor, useUnit } from "@/utils/style"
-export default {
-  name: "zm-text-highlight",
-  mixins: [hook, weixin],
-  props: {
-    text: {
-      type: [String, Number],
-      default: ""
-    },
-    textRow: {
-      type: [String, Number],
-      default: "2"
-    },
-    match: {
-      type: [String, Number],
-      default: ""
-    },
-    color: {
-      type: String,
-      default: ""
-    },
-    fontSize: {
-      type: [String, Number],
-      default: ""
-    },
-    fontWeight: {
-      type: [String, Number],
-      default: ""
-    },
-    lineHeight: {
-      type: [String, Number],
-      default: ""
-    },
-    highlightColor: {
-      type: String,
-      default: "var(--theme-color)"
-    },
-    customStyle: {
-      type: [Object, String],
-      default: ""
-    },
-    customClass: {
-      type: String,
-      default: ""
-    }
-  },
-  data() {
-    return {
-      list: []
-    }
-  },
-  computed: {
-    style() {
-      const style = {}
-      style.color = useColor(this.color)
-      style.fontSize = useUnit(this.fontSize)
-      style.fontWeight = this.fontWeight
-      style.lineHeight = this.lineHeight
-      style["-webkit-line-clamp"] = this.textRow
-      return useStyle({ ...style, ...useStyle(this.customStyle) })
-    },
-    textStyle() {
-      return (val) => {
-        const style = {}
-        if (val.matched) {
-          style.color = useColor(this.highlightColor)
-        }
-        return useStyle(style)
-      }
-    }
-  },
-  watch: {
-    text: {
-      handler() {
-        this.handleSplit()
-      },
-      immediate: true
-    },
-    match: {
-      handler() {
-        this.handleSplit()
-      },
-      immediate: true
-    }
-  },
-  created() {},
-  mounted() {},
-  methods: {
-    update() {
-      this.handleSplit()
-    },
-    handleSplit() {
-      const escapeRegExp = (string) => {
-        return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") // $& 表示匹配的整个字符串
-      }
-      if (this.text && isNoEmpty(this.match)) {
-        const escapedMatch = escapeRegExp(this.match)
-        const parts = this.text.split(new RegExp(`(${escapedMatch})`)).filter(Boolean)
-        this.list = parts.map((text) => {
-          return { text, matched: text === this.match }
-        })
-      } else {
-        this.list = [{ text: this.text, matched: false }]
-      }
-    }
+<script setup lang="ts">
+import { isNoEmpty } from "../utils/check"
+import { useStyle, useColor, useUnit } from "../hooks"
+
+const props = defineProps({
+  text: { type: String, default: "" },
+  match: { type: String, default: "" },
+  color: { type: String, default: "" },
+  textRow: { type: [String, Number], default: "2" },
+  fontSize: { type: [String, Number], default: "" },
+  fontWeight: { type: [String, Number], default: "" },
+  lineHeight: { type: [String, Number], default: "" },
+  highlightColor: { type: String, default: "var(--theme-color)" },
+  customStyle: { type: [Object, String], default: "" },
+  customClass: { type: String, default: "" },
+})
+
+const list = ref([])
+
+const style = computed(() => {
+  const style: any = {}
+  style.color = useColor(props.color)
+  style.fontSize = useUnit(props.fontSize)
+  style.fontWeight = props.fontWeight
+  style.lineHeight = props.lineHeight
+  style["-webkit-line-clamp"] = props.textRow
+  return useStyle({ ...style, ...useStyle(props.customStyle) })
+})
+
+const textStyle = computed(() => {
+  return (val: any) => {
+    const style: any = {}
+    if (val.matched) style.color = useColor(props.highlightColor)
+    return useStyle(style)
+  }
+})
+
+watch(() => props.text, handleSplit, { immediate: true })
+watch(() => props.match, handleSplit, { immediate: true })
+
+function update() {
+  handleSplit()
+}
+
+function handleSplit() {
+  const escapeRegExp = (val: string) => {
+    return val.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") // $& 表示匹配的整个字符串
+  }
+  if (props.text && isNoEmpty(props.match)) {
+    const escapedMatch = escapeRegExp(props.match)
+    const parts = props.text.split(new RegExp(`(${escapedMatch})`)).filter(Boolean)
+    list.value = parts.map((text: string) => {
+      return { text, matched: text === props.match }
+    })
+  } else {
+    list.value = [{ text: props.text, matched: false }]
   }
 }
-</script>
 
+defineExpose({ update })
+</script>
+<script lang="ts">
+export default {
+  options: { virtualHost: true, multipleSlots: true, styleIsolation: "shared" },
+}
+</script>
 <style lang="scss" scoped>
 .zm-text-highlight {
   width: 100%;
