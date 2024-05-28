3935bb1a3eb72fd6b865e4ab6bb617ccb0c1cfbc
李明扬 <1342554547@qq.com>
2024-05-28 17:56:30 +0800

    ⚡️【优化】组件改造

---
+++ src/ui/zm-icon-text/index.ts
@@ -0,0 +1,85 @@
+import IconText from "./zm-icon-text.vue"
+
+export const iconTextProps = {
+  /**
+   * @description 图标名称或者图片地址
+   */
+  name: { type: String, default: "" },
+  /**
+   * @description 图标大小
+   */
+  size: { type: [String, Number], default: "" },
+  /**
+   * @description 图标颜色
+   */
+  color: { type: String, default: "" },
+  /**
+   * @description 图标圆角
+   */
+  radius: { type: [String, Number], default: "" },
+  /**
+   * @description 图标粗细
+   */
+  weight: { type: String, default: "" },
+  /**
+   * @description 文本内容
+   */
+  text: { type: [String, Number], default: "" },
+  /**
+   * @description 文本大小
+   */
+  textSize: { type: [String, Number], default: "" },
+  /**
+   * @description 文本颜色
+   */
+  textColor: { type: String, default: "" },
+  /**
+   * @description 文本粗细
+   */
+  textWeight: { type: String, default: "" },
+  /**
+   * @description 文本和图标的间隔
+   */
+  textGap: { type: [String, Number], default: "" },
+  /**
+   * @description 文本在图标左边
+   */
+  textLeft: { type: Boolean, default: false },
+  /**
+   * @description 图标为图片时的宽度
+   */
+  width: { type: [String, Number], default: "" },
+  /**
+   * @description 图标为图片时的高度
+   */
+  height: { type: [String, Number], default: "" },
+  /**
+   * @description 图标为图片时的显示模式
+   */
+  imageMode: { type: String, default: "scaleToFill" },
+  /**
+   * @description 背景颜色
+   */
+  background: { type: String, default: "" },
+  /**
+   * @description 自定义图标名称前缀
+   */
+  customPrefix: { type: String, default: "zm-icon" },
+  /**
+   * @description 激活时的样式
+   */
+  customClass: { type: String, default: "" },
+  /**
+   * @description 自定义样式
+   */
+  customStyle: { type: [String, Object], default: "" },
+}
+export const iconTextEmits = {
+  click: (rect: TouchEvent) => true,
+}
+
+export type IconTextEmits = typeof iconTextEmits
+export type IconTextExpose = {
+  name: "zm-icon-text"
+}
+export type IconTextInstance = InstanceType<typeof IconText>
--- src/ui/zm-icon-text/zm-icon-text.vue
+++ src/ui/zm-icon-text/zm-icon-text.vue
@@ -1,170 +1,57 @@
 <template>
-  <view class="zm-icon-text" :class="[wrapClass, customClass]" :style="[wrapStyle]" @click="onClick">
+  <view class="zm-icon-text" :class="[classs, props.customClass]" :style="[style]" @click="click">
     <zm-icon :name="name" :size="size" :color="color" :radius="radius" :weight="weight" :custom-prefix="customPrefix"></zm-icon>
     <text v-if="text" class="icon-text" :style="[textStyle]">{{ text }}</text>
   </view>
 </template>
 
-<script>
-import hook from "@/mixins/hook"
-import weixin from "@/mixins/weixin"
-import { useStyle, useUnit, useColor } from "@/utils/style"
-/**
- * @name zm-icon 图标组件
- * @description 图标组件，支持字体图标和图片两种类型，可设置图标大小、颜色、圆角、粗细、文本等属性
- * @prop {String} name 图标名称或者图片地址
- * @prop {String|Number} size 图标大小
- * @prop {String} color 图标颜色
- * @prop {String|Number} radius 图标圆角
- * @prop {String} weight 图标粗细
- * @prop {String|Number} text 文本
- * @prop {String|Number} textSize 文本大小
- * @prop {String} textWeight 文本粗细
- * @prop {String} textColor 文本颜色
- * @prop {String|Number} textGap 文本间距
- * @prop {Boolean} textLeft 文本是否在图标左侧
- * @prop {String|Number} width 图标为图片时的宽度
- * @prop {String|Number} height 图标为图片时的高度
- * @prop {String} imageMode 图标为图片时的显示模式
- * @prop {String} background 背景颜色
- * @prop {String} customPrefix 自定义图标名称前缀
- * @prop {String|Array|Object} customClass 自定义类名
- * @prop {String|Object} customStyle 自定义样式
- */
-export default {
-  name: "zm-icon-text",
-  mixins: [hook, weixin],
-  props: {
-    // 图标名称或者图片地址
-    name: {
-      type: String,
-      default: ""
-    },
-    // 图标大小
-    size: {
-      type: [String, Number],
-      default: ""
-    },
-    // 图标颜色
-    color: {
-      type: String,
-      default: ""
-    },
-    // 图标圆角
-    radius: {
-      type: [String, Number],
-      default: ""
-    },
-    // 图标粗细
-    weight: {
-      type: String,
-      default: ""
-    },
-    // 文本
-    text: {
-      type: [String, Number],
-      default: ""
-    },
-    // 文本大小
-    textSize: {
-      type: [String, Number],
-      default: ""
-    },
-    // 文本粗细
-    textWeight: {
-      type: String,
-      default: ""
-    },
-    // 文本颜色
-    textColor: {
-      type: String,
-      default: ""
-    },
-    // 文本间距
-    textGap: {
-      type: [String, Number],
-      default: ""
-    },
-    // 文本是否在图标左侧
-    textLeft: {
-      type: Boolean,
-      default: false
-    },
-    // 图标为图片时的宽度
-    width: {
-      type: [String, Number],
-      default: ""
-    },
-    // 图标为图片时的高度
-    height: {
-      type: [String, Number],
-      default: ""
-    },
-    // 图标为图片时的显示模式
-    imageMode: {
-      type: String,
-      default: "scaleToFill"
-    },
-    // 背景颜色
-    background: {
-      type: String,
-      default: ""
-    },
-    // 自定义图标名称前缀
-    customPrefix: {
-      type: String,
-      default: "zm-icon"
-    },
-    // 自定义类名
-    customClass: {
-      type: [String, Array, Object],
-      default: ""
-    },
-    // 自定义样式
-    customStyle: {
-      type: [String, Object],
-      default: () => ({})
-    }
-  },
-  data() {
-    return {}
-  },
-  computed: {
-    wrapStyle() {
-      const style = {}
-      style.borderRadius = useUnit(this.radius)
-      style.background = useColor(this.background)
-      return useStyle({ ...style, ...useStyle(this.customStyle) })
-    },
-    wrapClass() {
-      const list = []
-      if (this.textLeft) list.push("zm-icon-text--left")
-      return list
-    },
-    // 文本样式
-    textStyle() {
-      const style = {}
-      style.color = useColor(this.textColor)
-      style.fontSize = useUnit(this.textSize)
-      style.fontWeight = this.textWeight
-      if (this.textGap) {
-        if (this.textLeft) style.marginRight = useUnit(this.textGap)
-        else style.marginLeft = useUnit(this.textGap)
-      }
-      if (this.textLeft) {
-        style.marginLeft = "0"
-      }
-      return useStyle(style)
-    }
-  },
-  methods: {
-    onClick() {
-      this.$emit("click")
-    }
+<script setup lang="ts">
+import { iconTextEmits, iconTextProps } from "./index"
+import { useStyle, useColor, useUnit } from "../hooks"
+
+defineOptions({ name: "zm-icon-text" })
+const emits = defineEmits(iconTextEmits)
+const props = defineProps(iconTextProps)
+
+const style = computed(() => {
+  const style: any = {}
+  style.borderRadius = useUnit(props.radius)
+  style.background = useColor(props.background)
+  return useStyle({ ...style, ...useStyle(props.customStyle) })
+})
+
+const classs = computed(() => {
+  const list = []
+  if (props.textLeft) list.push("zm-icon-text--left")
+  return list
+})
+
+const textStyle = computed(() => {
+  const style: any = {}
+  style.color = useColor(props.textColor)
+  style.fontSize = useUnit(props.textSize)
+  style.fontWeight = props.textWeight
+  if (props.textGap) {
+    if (props.textLeft) style.marginRight = useUnit(props.textGap)
+    else style.marginLeft = useUnit(props.textGap)
   }
+  if (props.textLeft) {
+    style.marginLeft = "0"
+  }
+  return useStyle(style)
+})
+
+function click(event: TouchEvent) {
+  emits("click", event)
+}
+
+defineExpose({ name: "zm-icon-text" })
+</script>
+<script lang="ts">
+export default {
+  options: { virtualHost: true, multipleSlots: true, styleIsolation: "shared" },
 }
 </script>
-
 <style lang="scss" scoped>
 .zm-icon-text {
   overflow: hidden;
@@ -173,13 +60,16 @@ export default {
   align-items: center;
   flex-direction: row;
   justify-content: center;
+
   &--left {
     flex-direction: row-reverse;
+
     .zm-icon-text__text {
       margin-left: 0;
       margin-right: 8rpx;
     }
   }
+
   .icon-text {
     margin-left: 8rpx;
   }
